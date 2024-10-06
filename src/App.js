6754ceaa0ea12a82b7ced06aec3deca16248daf8
Andrei Malets <coolviktori@yandex.by>
2024-10-07 00:20:04 +0300

    changes Cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Cards from "./components/Cards/Cards";
 import Header from "./components/Header";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Cards />
     </div>
   );
 };
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,7 +1,32 @@
 import React from "react";
-
+import img1 from "../../assets/images/cards/4.png";
+import img2 from "../../assets/images/cards/5.png";
+import img3 from "../../assets/images/cards/6.png";
+import img4 from "../../assets/images/cards/7.png";
+import img5 from "../../assets/images/cards/8.png";
+import img6 from "../../assets/images/cards/9.png";
+const initCards = [
+  { id: 1, name: "", img: img1 },
+  { id: 2, name: "", img: img2 },
+  { id: 3, name: "", img: img3 },
+  { id: 4, name: "", img: img4 },
+  { id: 5, name: "", img: img5 },
+  { id: 6, name: "", img: img6 },
+];
 const Cards = () => {
-  return <div>cards</div>;
+  return (
+    <div>
+      <div>
+        {initCards.map((card, index) => {
+          return (
+            <div key={card.id}>
+              <img alt="" src={card.img} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Cards;
